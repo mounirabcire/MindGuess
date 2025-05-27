98d64e2a52c20d9d9971d2bd2d71aa5de0682b9d
Mounir Abcire <mounirwebdevcode@gmail.com>
2025-05-27 12:12:57 +0100

    feat: add ImageBackground and LinearGradient components, update styling

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import { StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import StartGame from "./screens/startGame.screen";
 
+const diceBg = require("./assets/images/dices.jpg");
+
 export default function App() {
     return (
-        <View style={styles.appContainer}>
-            <StartGame />
-        </View>
+        <LinearGradient
+            style={styles.appContainer}
+            colors={["#6eeafa", "#E6E6EA"]}
+        >
+            <ImageBackground
+                style={styles.appContainer}
+                source={diceBg}
+                resizeMode="cover"
+                imageStyle={styles.bgImageStyle}
+            >
+                <StartGame />
+            </ImageBackground>
+        </LinearGradient>
     );
 }
 
 const styles = StyleSheet.create({
     appContainer: {
-        backgroundColor: "#E6E6EA",
         flex: 1,
+    },
+
+    bgImageStyle: {
+        opacity: 0.2,
     },
 });
 
