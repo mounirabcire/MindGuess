3b95fba11c7a02abbd242802c8893fb76c93401a
Mounir Abcire <mounirwebdevcode@gmail.com>
2025-06-01 11:06:32 +0100

    feat: add the logic game

--- screens/MiddleGame.screen.tsx
+++ screens/MiddleGame.screen.tsx
@@ -1,14 +1,76 @@
-import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Alert } from "react-native";
 
-function MiddleGame() {
+import Title from "../components/ui/Title";
+import NumberContainer from "../components/game/NumberContainer";
+import Button from "../components/ui/Button";
+import { generateRandomBetween } from "../utils/logic";
+
+interface MiddleGameI {
+    userNumber: number;
+    onGameOver: () => void;
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+function MiddleGame({ userNumber, onGameOver }: MiddleGameI) {
+    const initialGuessedNumber = generateRandomBetween(1, 100, userNumber);
+
+    const [guessedNumber, setGuessedNumber] = useState(initialGuessedNumber);
+    function lowerGuessHandler() {
+        // 21 > 5
+        if (userNumber > guessedNumber) {
+            Alert.alert("Sorry!", "I know you're lying!", [
+                { text: "Okay", style: "cancel" },
+            ]);
+            return;
+        }
+
+        maxBoundary = guessedNumber;
+
+        setGuessedNumber(
+            generateRandomBetween(minBoundary, maxBoundary, guessedNumber)
+        );
+    }
+
+    function higherGuessHandler() {
+        // 21 < 90
+        if (userNumber < guessedNumber) {
+            Alert.alert("Sorry!", "I know you're lying!", [
+                { text: "Okay", style: "cancel" },
+            ]);
+            return;
+        }
+
+        minBoundary = guessedNumber;
+
+        setGuessedNumber(
+            generateRandomBetween(minBoundary, maxBoundary, guessedNumber)
+        );
+    }
+
+    useEffect(() => {
+        if (userNumber === guessedNumber) onGameOver();
+    }, [userNumber, guessedNumber]);
+
     return (
         <View style={styles.screen}>
             <Title>Opponent's guess</Title>
 
-            <Text>Guess</Text>
+            <NumberContainer>{guessedNumber}</NumberContainer>
+
             <View>
-                <Text>Higher or lower?</Text>+ -
+                <Text style={styles.subText}>Higher or lower?</Text>
+
+                <View style={styles.btnContainer}>
+                    <Button type="primary" onGuess={higherGuessHandler}>
+                        +
+                    </Button>
+                    <Button type="primary" onGuess={lowerGuessHandler}>
+                        -
+                    </Button>
+                </View>
             </View>
             <View>
                 <Text>Log rounds</Text>
@@ -20,9 +82,20 @@ function MiddleGame() {
 const styles = StyleSheet.create({
     screen: {
         marginInline: 16,
-        marginTop: 80,
         padding: 16,
         flex: 1,
+    },
+
+    btnContainer: {
+        marginTop: 24,
+        flexDirection: "row",
+        gap: 16,
+    },
+
+    subText: {
+        marginTop: 24,
+        textAlign: "center",
+        fontSize: 24,
     },
 });
 
