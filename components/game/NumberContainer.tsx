383e65ff3df2bb1c919fdcee7a3db679bca66f00
Mounir Abcire <mounirwebdevcode@gmail.com>
2025-06-01 11:04:59 +0100

    feat: create NumberContainer component

---
+++ components/game/NumberContainer.tsx
@@ -0,0 +1,33 @@
+import { View, Text, StyleSheet } from "react-native";
+
+import colors from "../../utils/colors";
+
+interface NumberContainerI {
+    children: React.ReactNode;
+}
+
+function NumberContainer({ children }: NumberContainerI) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>{children}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 24,
+        padding: 32,
+        backgroundColor: colors["green-600"],
+        borderRadius: 4,
+    },
+
+    text: {
+        textAlign: "center",
+        color: colors.white,
+        fontSize: 32,
+        fontWeight: 800,
+    },
+});
+
+export default NumberContainer;
