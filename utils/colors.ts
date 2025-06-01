e53339b56d056e324d8929774c4e15a926c00a6a
Mounir Abcire <mounirwebdevcode@gmail.com>
2025-06-01 11:04:33 +0100

    feat: create colors and logic utils

---
+++ utils/colors.ts
@@ -0,0 +1,18 @@
+const colors = {
+    white: "#E6E6EA",
+    yellow: "#FED766",
+    red: "#FE4A49",
+
+    "green-300": "#57def0",
+    "green-400": "#2AB7CA",
+    "green-600": "#219fb3",
+    "green-700": "#2d7a85",
+    "green-800": "#1f8f9e",
+};
+
+export default colors;
+
+// white E6E6EA
+// yellow FED766
+// blue 2AB7CA
+// red FE4A49
