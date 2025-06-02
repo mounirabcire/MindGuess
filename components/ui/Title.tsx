f2c7ccde2405c2d1fcbf14baf704b707f535de35
Mounir Abcire <mounirwebdevcode@gmail.com>
2025-06-02 09:38:13 +0100

    feat: add custom fonts and accept custom styles as props

--- components/ui/Title.tsx
+++ components/ui/Title.tsx
@@ -1,20 +1,21 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, StyleProp, TextStyle } from "react-native";
 
 import colors from "../../utils/colors";
 
 interface TitleI {
     children: React.ReactNode;
+    style?: StyleProp<TextStyle>;
 }
 
-function Title({ children }: TitleI) {
-    return <Text style={styles.title}>{children}</Text>;
+function Title({ children, style }: TitleI) {
+    return <Text style={[styles.title, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
     title: {
         fontSize: 32,
         textAlign: "center",
-        fontWeight: 900,
+        fontFamily: "open-sans-bold",
         // color: colors["green-800"],
     },
 });
